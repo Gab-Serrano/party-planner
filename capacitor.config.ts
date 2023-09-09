10f4faf6a90fb57cc7c04b6a045a764dca3652f0
Gabriel Serrano <gab.serrano@duocuc.cl>
2023-09-08 23:47:01 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'party-planner',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
